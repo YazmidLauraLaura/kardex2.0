b069b5ab76bf602a7e9ef66d12610cc9e5a8eb54
Yazmid Laura Laura <yazmin25laura.laura@gmail.com>
2025-05-13 15:00:45 -0400

    Segundo commit

--- app.py
+++ app.py
@@ -57,7 +57,52 @@ def personas_save():
     conn.commit()
     conn.close()
     return redirect('/personas')
+   
+#crear un repositorio "kardex2.0" 
+#git init 
+#git add.
+#git commit -m"Primercommit"
+#pegar git hub
+#git remote -v
+#git status 
+#git push origin master 
+
+#editar 
+@app.route("/personas/edit/<int:id>")
+def persona_edit(id):
+    conn = sqlite3.connect("kardex.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM personas WHERE id =?", (id,))
+    persona = cursor.fetchone()
+    conn.close()
+    return render_template("personas/edit.html", persona=persona)
+
     
+@app.route("/personas/update", methods=['POST'])
+def personas_update():
+    id = request.form['id']
+    nombre = request.form['nombre']
+    telefono = request.form['telefono']
+    fecha_nac = request.form['fecha_nac']
+    
+    conn = sqlite3.connect("kardex.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("UPDATE personas SET nombre =?, telefono=?, fecha_nac=? WHERE id =?", (nombre, telefono, fecha_nac,id))
+    conn.commit()
+    conn.close()
+    return redirect("/personas")
+
+#eleiminar registro
+@app.route("/personas/delete/<int:id>")
+def personas_delete(id):
+    conn = sqlite3.connect("kardex.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM personas WHERE id =?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect('/personas')
 
 if __name__=="__main__":
     app.run(debug=True)
